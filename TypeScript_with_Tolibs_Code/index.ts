ebc3924a791fe15ea16bfc58eecdc9c9318cc805
dilmurodov <tolibdilmurodov98@gmail.com>
2022-02-27 13:39:05 +0500

    learing in progress

--- TypeScript_with_Tolibs_Code/index.ts
+++ TypeScript_with_Tolibs_Code/index.ts
@@ -58,5 +58,45 @@ sayHello({name: "Tolib", age: 19, language: 'ts'});
 // и если мы хотим возврашат любымий язык и она не передаль 
 // в качестве аргумента то он будет как undefined
 
+// если мы не хотим передать поля но создвать Custom type то используем Record<type1, type2, ...>
+
+// Например:
+
+// type Student = Record<string, number>
+
+// Если мы хотим удалить какое то поля из нашего Custom type то 
+// используем Omit< Название CutomType, 'Название ключ свойства'>
+// Например: 
+
+// type User = {
+//     name: string,
+//     sourname: string,
+// }
+
+// type NewUser = Omit<User, 'sourname'>
+
+// const SayHello = ({name, sourname}: NewUser):string => { // Ощибка,свойства sourname удалён
+//     return `Hello ${name}`;
+// }
+
+// Как можно сделать тип поли опцианальным? Используем Partial
+
+// Например:
+
+// type User = {
+//     name: string,
+//     sourname: string,
+// }
+
+// type NewUser = Partial<User>
+
+// // Это равно на:
+
+// type User = {
+//     name?: string,
+//     suourname?: string,
+// }
+
+// Интерфейсы:
 
 
