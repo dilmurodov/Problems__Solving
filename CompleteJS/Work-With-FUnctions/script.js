851631a4a02ad297830cf335d2a8c9dfc0a56ef2
dilmurodov <tolibdilmurodov98@gmail.com>
2022-03-10 04:51:02 +0500

    call/apply/bind

--- CompleteJS/Work-With-FUnctions/script.js
+++ CompleteJS/Work-With-FUnctions/script.js
@@ -169,6 +169,9 @@
 
 // Задача - 1
 
+// Создайте декоратор spy(func), который должен возвращать обёртку, которая сохраняет все вызовы функции в своём свойстве calls.
+// Каждый вызов должен сохраняться как массив аргументов.
+
 // function work(a, b){
 //   // console.log(this)
 //   console.log(a + b);
@@ -194,3 +197,173 @@
 // }
 
 // Задача - 2
+// Задерживающий декоратор
+// важность: 5
+// Создайте декоратор delay(f, ms), который задерживает каждый вызов f на ms миллисекунд. Например:
+
+
+// function f(x) {
+//   console.log(x);
+// }
+
+// function delay (f, ms) {
+//   return function () {
+//     setTimeout(() => {
+//       f.call(this, ...arguments);
+//     }, ms);
+//   };
+// }
+
+// // создаём обёртки
+// let f1000 = delay(f, 1000);
+// let f1500 = delay(f, 1500);
+
+// f1000("test"); // показывает "test" после 1000 мс
+// f1500("test"); // показывает "test" после 1500 мс
+
+// let f = debounce(console.log, 1000);
+
+// f(1); // выполняется немедленно
+// f(2); // проигнорирован
+
+// setTimeout( () => f(3), 100); // проигнорирован (прошло только 100 мс)
+// setTimeout( () => f(4), 700); // выполняется
+// setTimeout( () => f(5), 1100); // проигнорирован (прошло только 400 мс от последнего вызова)
+
+// function debounce(f, ms) {
+//   let isCooldown = false;
+
+//   // function wrapper(){
+//   if (isCooldown) return;
+//   f.apply(this, arguments);
+//   isCooldown = true;
+//   setTimeout(() => isCooldown = false, ms);
+// }
+// return wrapper;
+
+//   return function () {
+//     if(isCooldown) return;
+//     f.call(this, ...arguments);
+//     isCooldown = true;
+//     setTimeout(() => isCooldown = false, ms);
+//   }
+// }
+// lesson - 1
+
+// function sum(a){
+//   return function (b){
+//     return a + b;
+//   }
+// }
+// console.log(sum(2)(3));
+
+
+// function sum(a, b){
+//   return a + b;
+// }
+
+// console.log(sum(1, (function(a, b) {
+//   return a + b;})(1, 2)));
+
+// function count(){
+//   let counter = 0;
+//   return function(){
+//     return counter++;
+//   }
+// }
+
+// let c = count();
+// console.log(c());
+// console.log(c());
+// console.log(c());
+
+// Functions Accepting Callback functions
+
+// let word = prompt('Gapni kiriting:');
+
+// function kattaHarf(str){
+//   return str.toUpperCase();
+// }
+
+// function kichikHarf(str){
+//   return str.toLowerCase();
+// }
+
+// function birinchiHarf(str){
+//   str = str.toLowerCase();
+//   let arr = str.split(' ');
+//   arr.forEach((item, index) => {
+//     arr[index] = item[0].toUpperCase() + item.slice(1);
+//   });
+
+//   return arr.join(' ');
+// }
+
+// function camelCase(str){
+//   str = str.toLowerCase();
+//   let arr = str.split(' ');
+//   arr.forEach((item, index) => {
+//     arr[index] = item[0].toUpperCase() + item.slice(1);
+//   });
+
+//   return arr.join('')[0].toLowerCase() + arr.join('').slice(1);
+
+// }
+
+// function transfrom (word, f){
+//   return f(word);
+// }
+// console.log(transfrom(word, kattaHarf));
+// console.log(transfrom(word, kichikHarf));
+// console.log(transfrom(word, birinchiHarf));
+// console.log(transfrom(word, camelCase));
+
+// Functions Returning Functions
+
+// function sum(f){
+//   return function(a){
+//     return function(b){
+//       return f(a, b);
+//     }
+//   }
+// }
+// let f = function (a, b){
+//   return a+b;
+// }
+// let createSumm = sum(f);
+
+// // let add = createSumm(1);
+
+// // console.log(add(2));
+
+// console.log(createSumm(1)(2))
+
+// bind => function which his this is binded
+
+'use strict'
+
+// function compose(f, g){
+//   return function( ...args){
+//     return f.call(this, g.call(compose, ...args));
+//   };
+// }
+
+// const sum = function (x, y) {return x+y};
+
+// const square = x => x*x;
+
+// let res = compose(square, sum)(2, 3);
+
+// console.log(res)
+
+
+const person = {
+  count: 0,
+  counter(){
+    this.count++;
+    console.log(this.count);
+  }
+}
+
+let click = person.counter;
+click.call(person);
