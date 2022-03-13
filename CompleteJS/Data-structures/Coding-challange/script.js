98811093b306ac11e3260f5967da20f2dc55e4fe
dilmurodov <tolibdilmurodov98@gmail.com>
2022-03-13 14:20:17 +0500

    js in browser

--- CompleteJS/Data-structures/Coding-challange/script.js
+++ CompleteJS/Data-structures/Coding-challange/script.js
@@ -1,90 +1,94 @@
-// Coding Challenge #2
+// // Coding Challenge #2
 
-/* 
-Let's continue with our football betting app!
-1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
-2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
-3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
-      Odd of victory Bayern Munich: 1.33
-      Odd of draw: 3.25
-      Odd of victory Borrussia Dortmund: 6.5
-Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
-BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
-      {
-        Gnarby: 1,
-        Hummels: 1,
-        Lewandowski: 2
-      }
-GOOD LUCK 😀
-*/
+// /* 
+// Let's continue with our football betting app!
+// 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+// 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+// 3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+//       Odd of victory Bayern Munich: 1.33
+//       Odd of draw: 3.25
+//       Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+// BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+//       {
+//         Gnarby: 1,
+//         Hummels: 1,
+//         Lewandowski: 2
+//       }
+// GOOD LUCK 😀
+// */
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 
-//1* ==============================
+// //1* ==============================
 
-for (let i in game.scored) {
-  console.log(`Goal ${+i + 1}: ${game.scored[i]}`)
-}
+// for (let i in game.scored) {
+//   console.log(`Goal ${+i + 1}: ${game.scored[i]}`)
+// }
 
-//*2 ==============================
+// //*2 ==============================
 
-let odds = Object.values(game.odds);
-console.log(+(odds.reduce((acc, item) => acc + item, 0) / odds.length).toFixed(2))
+// let odds = Object.values(game.odds);
+// console.log(+(odds.reduce((acc, item) => acc + item, 0) / odds.length).toFixed(2))
 
-//*3 ==============================
-// Используем сокрашенное вычесление
+// //*3 ==============================
+// // Используем сокрашенное вычесление
 
-for (let [key, val] of Object.entries(game.odds)) {
-  console.log(`Odd of ${game[key] ? 'victory' : 'draw' } ${game[key] || ''}: ${val}`)
-}
+// for (let [key, val] of Object.entries(game.odds)) {
+//   console.log(`Odd of ${game[key] ? 'victory' : 'draw' } ${game[key] || ''}: ${val}`)
+// }
 
-//*4 ==============================
+// //*4 ==============================
 
-scorers = {};
+// scorers = {};
 
-for(let i of game.scored){
-  scorers[i] ? scorers[i]++ : scorers[i] = 1;
-}
+// for(let i of game.scored){
+//   scorers[i] ? scorers[i]++ : scorers[i] = 1;
+// }
 
-console.log(scorers)
+// console.log(scorers)
 
-/// Complated 
+// /// Complated 
+
+$.getJSON('https://json.geoiplookup.io/?callback=?', function(data) {
+  console.log(JSON.stringify(data, null, 2));
+});
